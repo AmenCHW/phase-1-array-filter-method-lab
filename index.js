e3417941e5e32ee60249b80206bb1cef8ea923d5
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-28 13:41:54 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+let findMatching = (drivers, name)=> {
+    return drivers.filter(driver => driver.toLowerCase() == name.toLowerCase())
+}
+
+let fuzzyMatch = (drivers, name)=> {
+    return drivers.filter(driver => driver.charAt(0) == name.charAt(0))
+}
+
+let matchName = (drivers, name) => {
+  return drivers.filter(driver => driver.name === name)
+}
